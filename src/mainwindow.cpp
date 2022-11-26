ef330e5f904501bcfd41e02279ce8f2d94eddcfc
Uros Ljubisavljevic <ljubisavljevicu@gmail.com>
2022-11-26 13:58:30 +0000

    Implementiran right click na tastere, dodata SoundButton klasa

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -6,6 +6,22 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    connect(ui->pbQ, &SoundButton::rightClicked, this, &MainWindow::onRightClicked);
+    connect(ui->pbW, &SoundButton::rightClicked, this, &MainWindow::onRightClicked);
+    connect(ui->pbE, &SoundButton::rightClicked, this, &MainWindow::onRightClicked);
+    connect(ui->pbR, &SoundButton::rightClicked, this, &MainWindow::onRightClicked);
+
+    connect(ui->pbA, &SoundButton::rightClicked, this, &MainWindow::onRightClicked);
+    connect(ui->pbS, &SoundButton::rightClicked, this, &MainWindow::onRightClicked);
+    connect(ui->pbD, &SoundButton::rightClicked, this, &MainWindow::onRightClicked);
+    connect(ui->pbF, &SoundButton::rightClicked, this, &MainWindow::onRightClicked);
+
+    connect(ui->pbZ, &SoundButton::rightClicked, this, &MainWindow::onRightClicked);
+    connect(ui->pbX, &SoundButton::rightClicked, this, &MainWindow::onRightClicked);
+    connect(ui->pbC, &SoundButton::rightClicked, this, &MainWindow::onRightClicked);
+    connect(ui->pbV, &SoundButton::rightClicked, this, &MainWindow::onRightClicked);
+
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +29,7 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::onRightClicked(SoundButton *button)
+{
+    std::cout << "User right clicked " + button->text().toStdString() << std::endl;
+}
