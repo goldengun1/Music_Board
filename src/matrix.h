1b3ba11d41fc2e4bd39ff2ddc06d7d8a238dc75f
Aleksandar Urosevic <mi19201@alas.matf.bg.ac.rs>
2022-11-27 18:08:32 +0100

    Dopusti javni pristup za Matrix::timeline, Matrix je sada struct

--- src/matrix.h
+++ src/matrix.h
@@ -4,11 +4,10 @@
 #include <QString>
 #include "soundbank.h" // For "sid" type.
 
-class Matrix
+struct Matrix
 {
     std::vector<std::pair<uint32_t, sid>> timeline;
 
-public:
     void     Clear(void);
     uint32_t Append(std::pair<uint32_t, sid>);
     uint32_t Append(uint32_t, sid);
