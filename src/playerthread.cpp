a70f02a5937a8b82f9a6f674ce34c8356a3a1f88
Aleksandar Urosevic <mi19201@alas.matf.bg.ac.rs>
2022-12-01 22:23:57 +0100

    Koristi precizno vreme za PlayerThread

--- src/playerthread.cpp
+++ src/playerthread.cpp
@@ -1,14 +1,19 @@
 #include "playerthread.h"
 
 #include <iostream>
+#include <QDebug>
 
 PlayerThread::PlayerThread(const Matrix& matrix, QObject *parent) : QThread(parent), matrix{matrix} {
 }
 
 void PlayerThread::run() {
     uint32_t elapsed = 0;
+    QElapsedTimer timer;
+
+    timer.start();
     while (!matrix.timeline.empty()) {
-        while (!matrix.timeline.empty() && matrix.timeline.top().first == elapsed) {
+        while (!matrix.timeline.empty() && matrix.timeline.top().first <= elapsed) {
+            qDebug() << matrix.timeline.top().first << " <= " << elapsed;
             std::cout << "Mark hit: " << elapsed << ' ';
             emit markHit(matrix.timeline.top().second);
             matrix.timeline.pop();
@@ -16,8 +21,7 @@ void PlayerThread::run() {
 
         if (matrix.timeline.empty()) break; // Proveravamo drugi put da li je prazan da ne bismo radili sleep ako nije potrebno.
 
-        QThread::msleep(1);
-        elapsed++;
+        elapsed = timer.elapsed();
     }
 
     emit playFinished();
